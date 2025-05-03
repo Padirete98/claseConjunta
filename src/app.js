0aad575e71e40fe52a3f262f4d98dc9df4b6deb3
tessita <tere.reyna.pa.98@gmail.com>
2025-05-03 12:31:15 -0600

    server working

--- src/app.js
+++ src/app.js
@@ -6,4 +6,11 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+app.get('/', (req, res) =>{
+    res.send('Hello World!');
 
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
