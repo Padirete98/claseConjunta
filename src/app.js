2c6880bd5e8880a6934133745f2a397c7aaadef6
tessita <tere.reyna.pa.98@gmail.com>
2025-05-03 12:22:43 -0600

    add .env

--- src/app.js
+++ src/app.js
@@ -1,1 +1,9 @@
-console.log("hola Mundo");
+import express from 'express';
+import dotenv from 'dotenv'; //libreria para leer el archivo .env
+
+dotenv.config(); //nos instruye al puerto
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+
